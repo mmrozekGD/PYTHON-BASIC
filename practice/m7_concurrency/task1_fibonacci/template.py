001cfcc991b1598d1f6c9b629677ebbdbfc2f06f
Mateusz Mrozek <mmrozek@griddynamics.com>
2026-06-25 16:11:47 +0200

    module 7 completed

--- practice/m7_concurrency/task1_fibonacci/template.py
+++ practice/m7_concurrency/task1_fibonacci/template.py
@@ -1,31 +1,88 @@
 import os
 from random import randint
+from pathlib import Path
+import queue
+from queue import Queue
+from threading import Thread
+import sys
 
-
-OUTPUT_DIR = './output'
-RESULT_FILE = './output/result.csv'
+CURR_DIR = Path(__file__).parent
+OUTPUT_DIR = CURR_DIR / "output"
+RESULT_FILE = OUTPUT_DIR / "result.csv"
+NUM_WORKERS = 11
 
 
 def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
 
     f0, f1 = 0, 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         f0, f1 = f1, f0 + f1
     return f1
 
 
+def worker1(task_q: Queue, output_dir):
+    while True:
+        try:
+            task = task_q.get(block=False)
+        except queue.Empty:
+            return
+
+        result = fib(task)
+        with open(output_dir / f"{task}.txt", "w") as f:
+            f.write(str(result))
+
+        task_q.task_done()
+
+
 def func1(array: list):
-    pass
+    tasks = Queue()
+    for num in array:
+        tasks.put(num)
+
+    threads = [
+        Thread(target=worker1, args=(tasks, OUTPUT_DIR)) for _ in range(NUM_WORKERS)
+    ]
+
+    [t.start() for t in threads]
+
+    tasks.join()
+
+
+def worker2(task_q: Queue, result_file: str):
+    while True:
+        try:
+            task = task_q.get()
+        except task_q.empty:
+            return
+        str_to_write = task[0] + "," + task[1] + "\n"
+        with open(result_file, "a") as f:
+            f.write(str_to_write)
 
 
 def func2(result_file: str):
-    pass
+    tasks2 = Queue()
+    for file in Path(result_file).parent.iterdir():
+        name = file.name
+        ord_num = name.split(".")[0]
+        with open(file) as f:
+            fib_num = f.read()
+        tasks2.put((ord_num, fib_num))
+
+    threads = [
+        Thread(target=worker2, args=(tasks2, result_file)) for _ in range(NUM_WORKERS)
+    ]
+
+    [t.start() for t in threads]
+
+    tasks2.join()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    func1(array=[randint(1000, 100000) for _ in range(1000)])
+    sys.set_int_max_str_digits(30000)
+
+    func1(array=[randint(1000, 100000) for _ in range(1000)])  # 1000
     func2(result_file=RESULT_FILE)
--- practice/m7_concurrency/task2_apod_api/template.py
+++ practice/m7_concurrency/task2_apod_api/template.py
@@ -1,24 +1,96 @@
-API_KEY = "YOUR_KEY"
-APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
-OUTPUT_IMAGES = './output'
+import keyring
+import requests
+import json
+import os
+from pathlib import Path
+from queue import Queue, Empty
+from threading import Thread
+
+# API_KEY = "YOUR_KEY" # quite funny
+APOD_ENDPOINT = "https://api.nasa.gov/planetary/apod"
+CURR_DIR = Path(__file__).parent
+OUTPUT_DIR = CURR_DIR / "output"
+NUM_WORKERS = 11
+
+
+def get_apod_metadata_one_day(date: str, api_key: str):
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={date}"
+
+    response = requests.get(url=url)
+
+    return [response]
 
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&start_date={start_date}&end_date={end_date}"
+
+    response = requests.get(url=url)
+
+    if response.status_code != 200:
+        raise Exception(
+            f"Connection error while calling API. Staus code: {response.status_code}"
+        )
+
+    response = json.loads(response.content)
+    return response
+
+
+def worker_downloader(task_q: Queue):
+    while True:
+        try:
+            meta = task_q.get(block=False)
+        except Empty:
+            return
+        date = meta["date"]
+        if meta["media_type"] != "image":
+            print(f"There is no photo for {date}. Skipping")
+            task_q.task_done()
+            continue
+        image_url = meta["url"]
+
+        r = requests.get(image_url)
+        if r.status_code == 200:
+            with open(OUTPUT_DIR / f"{date}.jpg", "wb") as f:
+                f.write(r.content)
+        else:
+            print(
+                f"Problem with downloading photo for {date}. Status code: {r.status_code}"
+            )
+        task_q.task_done()
 
 
 def download_apod_images(metadata: list):
-    pass
+    tasks_q = Queue()
+    for meta in metadata:
+        tasks_q.put(meta)
+
+    threads = [
+        Thread(target=worker_downloader, args=(tasks_q,)) for _ in range(NUM_WORKERS)
+    ]
+
+    [t.start() for t in threads]
+
+    tasks_q.join()
 
 
 def main():
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    api_key = keyring.get_password("nasa_concurrent_task", "NASA_API_KEY")
+
+    # metadata = get_apod_metadata_one_day(date="2021-08-01", api_key=api_key)
+
+    # download_apod_images(metadata)
+
     metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
-        api_key=API_KEY,
+        start_date="2021-08-01",
+        end_date="2021-09-30",
+        api_key=api_key,
     )
     download_apod_images(metadata=metadata)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
